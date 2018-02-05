2aa98c3ca9a4b6083f620e4d702ccd098949c211
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-05 17:27:49 +0530

    commit

--- Hiru_Issues/lambda.js
+++ Hiru_Issues/lambda.js
@@ -1,31 +1,10 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-
-
-ddb.get({
-	TableName: 'samplehiru',
-	Key: {
-		'1': '1'
-	}
-}, function (err, data) {
-	if (err) {
-		//handle error
-	} else {
-		//your logic goes here
-	}
-});
-
-
-
-
-
-
-
-
-
 exports.handler = function (event, context, callback) {
+
 
 
 	callback(null, 'Successfully executed');
 }
+
