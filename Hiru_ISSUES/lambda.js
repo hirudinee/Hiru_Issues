e5f066a38e54eed0c0931adb9a4c6e945d602257
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-14 15:58:58 +0530

    remove sqs

--- Hiru_ISSUES/lambda.js
+++ Hiru_ISSUES/lambda.js
@@ -2,21 +2,7 @@ let AWS = require('aws-sdk');
 let SL = require('@slappforge/slappforge-sdk');
 const sqs = new SL.AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
-	sqs.sendMessage({
-		MessageBody: 'your message body should be assigned herezxx',
-		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/263248768798/Test',
-		DelaySeconds: '0',
-		MessageAttributes: {
-			"test": {
-				"DataType": "String",
-				"StringValue": "hiru"
-			}
-		}
-	}, function (data) {
-		console.log(data);
-	}, function (error) {
-		console.log(error);
-	});
+	
 
 
 	callback(null, 'Successfully executed');
